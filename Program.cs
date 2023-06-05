f0b39dafe0b26ced6019a1ae3d003ecbcfaeea3b
Pedro <pedrogarrido971@gmail.com>
2023-06-05 14:11:52 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] provincias ={"Santa Fe", "Tierra del fuego", "Chubut", "Río Negro", "La Pampa",
+                            "Neuquen", "La Rioja", "Buenos Aires", "Entre Ríos", "Corrientes",
+                            "Santiago del Estero", "Tucuman", "Córdoba", "San Luis", "Catamarca",
+                            "Chaco", "Formosa", "Jujuy", "Salta", "Misiones", "Santa Cruz",
+                            "Mendoza", "San Juan" };
+
+        var misProvincias = from p in provincias where p.StartsWith("S")||p.StartsWith("T") select p;
+
+        
+        foreach(var prov in misProvincias)
+        {
+            Console.WriteLine(prov);
+        }
+
+        Console.ReadKey();
+    }
+}
